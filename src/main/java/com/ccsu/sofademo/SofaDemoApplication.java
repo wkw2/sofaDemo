668f154bb3283e1ebc83086de2eca09a528fd6dd
“wk” <1741443247@qq.com>
2024-05-21 20:22:02 +0800

    sofaTest

---
+++ src/main/java/com/ccsu/sofademo/SofaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ccsu.sofademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaDemoApplication.class, args);
+    }
+
+}
